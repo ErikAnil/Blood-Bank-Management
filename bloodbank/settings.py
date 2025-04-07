dde9fec3b5aef399d29ca189a2084a2ce0aa3900
Erik Anil <anilerik21@gmail.com>
2025-04-07 17:44:24 +0100

    Fix static file configuration for production

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -116,13 +116,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+# settings.py
 
 STATIC_URL = '/static/'
+
+# For production, use STATIC_ROOT to collect all static files in one place
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Use STATICFILES_DIRS for any additional static files during development (optional)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'bloodbank/static'),
+    os.path.join(BASE_DIR, 'bloodbank/static'),
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this middleware for static file handling
+    # Other middlewares
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
